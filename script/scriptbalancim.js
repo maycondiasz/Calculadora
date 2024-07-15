e5d1443609dd440250dc1ec8ddbc97296bf87d35
Maycon Dias <clovismaycon@gmail.com>
2024-07-15 16:13:40 +0000

    Ajustes fachadeiro

--- script/scriptbalancim.js
+++ script/scriptbalancim.js
@@ -28,7 +28,7 @@ document.getElementById('afastador').innerHTML = balancim1*2 + " Afastador";
 document.getElementById('guincho').innerHTML = balancim1*2 + " Guincho";
 document.getElementById('cabide').innerHTML = balancim1*2 + " Cabide";
 document.getElementById('cabos').innerHTML = balancim1*4 + " Cabos";
-document.getElementById('clipes').innerHTML = balancim1*16 + " Clipes";
+document.getElementById('clipes').innerHTML = balancim1*12 + " Clipes";
 document.getElementById('parafusos').innerHTML = balancim1*12 + " Parafusos";
 }
 
@@ -40,7 +40,7 @@ document.getElementById('afastador1m').innerHTML = balancim1m*2 + " Afastador";
 document.getElementById('guincho1m').innerHTML = balancim1m*2 + " Guincho";
 document.getElementById('cabide1m').innerHTML = balancim1m*2 + " Cabide";
 document.getElementById('cabos1m').innerHTML = balancim1m*4 + " Cabos";
-document.getElementById('clipes1m').innerHTML = balancim1m*16 + " Clipes";
+document.getElementById('clipes1m').innerHTML = balancim1m*12 + " Clipes";
 document.getElementById('parafusos1m').innerHTML = balancim1m*12 + " Parafusos";
 }
 
@@ -52,7 +52,7 @@ document.getElementById('afastador2M').innerHTML = balancim2*2 + " Afastador";
 document.getElementById('guincho2M').innerHTML = balancim2*2 + " Guincho";
 document.getElementById('cabide2M').innerHTML = balancim2*2 + " Cabide";
 document.getElementById('cabos2M').innerHTML = balancim2*4 + " Cabos";
-document.getElementById('clipes2M').innerHTML = balancim2*16 + " Clipes";
+document.getElementById('clipes2M').innerHTML = balancim2*12 + " Clipes";
 document.getElementById('parafusos2M').innerHTML = balancim2*12 + " Parafusos";
 }
 
@@ -64,7 +64,7 @@ document.getElementById('afastador3').innerHTML = balancim3*2 + " Afastador";
 document.getElementById('guincho3').innerHTML = balancim3*2 + " Guincho";
 document.getElementById('cabide3').innerHTML = balancim3*2 + " Cabide";
 document.getElementById('cabos3').innerHTML = balancim3*4 + " Cabos";
-document.getElementById('clipes3').innerHTML = balancim3*16 + " Clipes";
+document.getElementById('clipes3').innerHTML = balancim3*12 + " Clipes";
 document.getElementById('parafusos3').innerHTML = balancim3*12 + " Parafusos";
 }
 
@@ -77,7 +77,7 @@ document.getElementById('guincho4').innerHTML = balancim4*2 + " Guincho";
 document.getElementById('cabide4').innerHTML = balancim4*2 + " Cabide";
 document.getElementById('uniao4').innerHTML = balancim4 + " União";
 document.getElementById('cabos4').innerHTML = balancim4*4 + " Cabos";
-document.getElementById('clipes4').innerHTML = balancim4*16 + " Clipes";
+document.getElementById('clipes4').innerHTML = balancim4*12 + " Clipes";
 document.getElementById('parafusos4').innerHTML = balancim4*20 + " Parafusos";
 }
 
@@ -90,7 +90,7 @@ document.getElementById('guincho5').innerHTML = balancim5*2 + " Guincho";
 document.getElementById('cabide5').innerHTML = balancim5*2 + " Cabide";
 document.getElementById('uniao5').innerHTML = balancim5 + " União";
 document.getElementById('cabos5').innerHTML = balancim5*4 + " Cabos";
-document.getElementById('clipes5').innerHTML = balancim5*16 + " Clipes";
+document.getElementById('clipes5').innerHTML = balancim5*12 + " Clipes";
 document.getElementById('parafusos5').innerHTML = balancim5*20 + " Parafusos";
 }
 
@@ -103,6 +103,6 @@ document.getElementById('guincho6').innerHTML = balancim6*2 + " Guincho";
 document.getElementById('cabide6').innerHTML = balancim6*2 + " Cabide";
 document.getElementById('uniao6').innerHTML = balancim6 + " União";
 document.getElementById('cabos6').innerHTML = balancim6*4 + " Cabos";
-document.getElementById('clipes6').innerHTML = balancim6*16 + " Clipes";
+document.getElementById('clipes6').innerHTML = balancim6*12 + " Clipes";
 document.getElementById('parafusos6').innerHTML = balancim6*20 + " Parafusos";
 }
--- script/scriptfachadeiro.js
+++ script/scriptfachadeiro.js
@@ -1,19 +1,34 @@
-let inputmetragem ;
+let inputlargura ;
+let inputaltura ;
 let fachadeiro ;
+let eve ;
+let evs ;
+let lances;
 let pisos ;
 let bl;
 let dx;
-function calcularFachadeiro() {
-  inputmetragem = document.getElementById("metragem").value
-  if (inputmetragem % 2 != 0) {
-    inputmetragem = parseInt(inputmetragem)+1
+function calcularFachadeiro(largura, altura) {
+  inputlargura = document.getElementById(largura).value
+  if (inputlargura % 2 != 0) {
+    inputlargura = parseInt(inputlargura)+1
   }
+
+  inputaltura = document.getElementById(altura).value
+  if (inputaltura % 2 != 0) {
+    inputaltura = parseInt(inputaltura)+1
+  }
+  lances = inputaltura / 2;
+  fachadeiro = ((inputlargura/2)+1) * lances;
+  pisos = ((inputlargura/2)*3) * lances;
+  dx = (inputlargura/2) * lances;
+  bl = inputlargura * lances;
+  eve = (lances * 2);
+  evs = fachadeiro - (lances * 2);
+
   
-  fachadeiro = (inputmetragem/2)+1;
-  pisos = (inputmetragem/2)*3;
-  dx = inputmetragem/2;
-  bl = inputmetragem;
-  document.getElementById('quantfachadeiro').innerHTML = fachadeiro + " fachadeiros";
+  
+document.getElementById('lances').innerHTML = `<b>Estrutura com ${lances} lances</b>`;
+  document.getElementById('quantfachadeiro').innerHTML = `${fachadeiro} fachadeiros</br> (${eve} EVE e ${evs} EVS)`;
   document.getElementById('quantpisos').innerHTML = pisos + " pisos";
 
   document.getElementById('quantbldx').innerHTML = dx + " DX e, " + bl + " BL";
